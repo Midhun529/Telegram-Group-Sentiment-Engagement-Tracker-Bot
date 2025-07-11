cb8c03df08de97fd46d6ecd70e4e830a8416108c
Midhun Chandran <midhunchandran@Midhuns-Laptop.local>
2025-07-11 17:54:15 +1000

    First commit
    Bot file read and respond to the messages

---
+++ bot.py
@@ -0,0 +1,9 @@
+
+from telegram.ext import *
+import responses
+
+
+#Bot user_name and API token to connect the program to the code
+
+bot_user_name ="SentimentTracker_bot"
+API_token="8129880551:AAFTmwamjgYbHs7xHPRP1p9mfK6QqJN_vic" 
